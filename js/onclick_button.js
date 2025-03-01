57289a42e259acf1e6c050b8edbd64048f2ad251
sahilgangani17 <sg17022007@gmail.com>
2025-03-02 01:26:44 +0530

    Addede Files

--- js/onclick_button.js
+++ js/onclick_button.js
@@ -1,21 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth,  signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
-// Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCuGCAAFiiMDrWeerT0eY-ZzcbpeemBa8M",
-  authDomain: "carbonari-b1a91.firebaseapp.com",
-  projectId: "carbonari-b1a91",
-  storageBucket: "carbonari-b1a91.firebasestorage.app",
-  messagingSenderId: "487055169038",
-  appId: "1:487055169038:web:4114c67d398b5765d0ce5f",
-  measurementId: "G-N3H2V77CN3"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
 
 
 function dologin(){
@@ -48,17 +31,3 @@ function gosameindex(){
     window.location.href = "index.html";
 }
 
-async function dologout(){
-    try {
-        await signOut(auth);
-        alert('You have been logged out!');
-        localStorage.removeItem('LoggedUserID');
-        localStorage.removeItem('LoggedUserEmail');
-        localStorage.removeItem('LoggedUsername');
-        localStorage.removeItem('UserSaveFile');
-        window.location.href = 'index.html';  
-      } catch (error) {
-        console.error('Error logging out:', error);
-        alert('There was an error logging you out. Please try again.');
-      }
-}
